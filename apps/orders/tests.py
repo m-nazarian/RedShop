8ffb720484736d1903b23af4037781f70846a0e5
Mohammad <pythonnazarian@gmail.com>
2026-07-12 23:11:43 +0330

    Add payment process state tests

--- apps/orders/tests.py
+++ apps/orders/tests.py
@@ -260,6 +260,48 @@ class CheckoutSecurityTests(RedShopTestBase, TestCase):
         self.assertNotIn(CHECKOUT_ORDER_SESSION_KEY, session)
 
 
+
+    def test_payment_process_clears_session_for_already_paid_order(self):
+        order, _payment_method = self._create_paid_stale_order()
+        order.payment_method = "online"
+        order.paid = True
+        order.stock_released = False
+        order.save(update_fields=["payment_method", "paid", "stock_released", "updated"])
+
+        session = self.client.session
+        session[PAYMENT_ORDER_SESSION_KEY] = order.id
+        session[CHECKOUT_ORDER_SESSION_KEY] = order.id
+        session.save()
+
+        response = self.client.get(reverse("payment:process"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "payment/success.html")
+
+        session = self.client.session
+        self.assertNotIn(PAYMENT_ORDER_SESSION_KEY, session)
+        self.assertNotIn(CHECKOUT_ORDER_SESSION_KEY, session)
+
+    def test_payment_process_clears_session_for_stock_released_order(self):
+        order, _payment_method = self._create_paid_stale_order()
+        order.payment_method = "online"
+        order.paid = False
+        order.stock_released = True
+        order.save(update_fields=["payment_method", "paid", "stock_released", "updated"])
+
+        session = self.client.session
+        session[PAYMENT_ORDER_SESSION_KEY] = order.id
+        session[CHECKOUT_ORDER_SESSION_KEY] = order.id
+        session.save()
+
+        response = self.client.get(reverse("payment:process"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "payment/failure.html")
+
+        session = self.client.session
+        self.assertNotIn(PAYMENT_ORDER_SESSION_KEY, session)
+        self.assertNotIn(CHECKOUT_ORDER_SESSION_KEY, session)
     def test_cart_mutation_views_are_post_only(self):
         update_response = self.client.get(reverse("cart:update_quantity"))
         remove_response = self.client.get(reverse("cart:remove_item"))
